b76614800c5a74af6cd55cdd0c77c3f89e24ff53
0x3l <89667887+0x3l@users.noreply.github.com>
2021-08-29 13:49:17 -0300

    b
    b

--- loop.ps1
+++ loop.ps1
@@ -1,6 +1,13 @@
-$i = 360
+$i = 0
+while($True) {
+    Write-Output $i
+    Sleep 60
+    $i++
+}
+
+<#$i = 360
 do {
     Write-Host $i
     Sleep 60
     $i--
-} while ($i -gt 0)
+} while ($i -gt 0)#>

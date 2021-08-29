e460b547072f56db4e1bb8293bb64362f01580ad
0x3l <89667887+0x3l@users.noreply.github.com>
2021-08-29 00:03:17 -0300

    b
    b

--- user.bat
+++ user.bat
@@ -7,13 +7,19 @@ REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Ad
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V LaunchTo /T REG_DWORD /D 1 /F
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /V AppsUseLightTheme /T REG_DWORD /D 0 /F
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /V SystemUsesLightTheme /T REG_DWORD /D 0 /F
-REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /V WallPaper /T REG_SZ /D "D:\a\Maybe-Lol\Maybe-Lol\wlp.png" /F
 REG ADD "HKEY_CURRENT_USER\Control Panel\International" /V sShortDate /T REG_SZ /D "M/d/yy" /F
 
 :: Curl
+cd ..\..\..
+mkdir TEMP_Downloads
+cd TEMP_Downloads
 curl -O https://cdn.discordapp.com/attachments/881361225756332032/881361451451813918/wlp.png
 curl -O https://cdn.discordapp.com/attachments/881361225756332032/881364827405168660/Radmin_VPN.exe
 curl -O https://cdn.discordapp.com/attachments/881361225756332032/881365133346107444/RAdmin.txt
+
+:: Custom
+
+REG ADD "HKEY_CURRENT_USER\Control Panel\Desktop" /V WallPaper /T REG_SZ /D "D:\TEMP_Downloads\wlp.png" /F
 
 :: Run
 Radmin_VPN.exe
